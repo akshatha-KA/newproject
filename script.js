0e79c44988c68b0aad903c83cd96518c6073fcc0
akshatha-KA <kumariakshatha6@gmail.com>
2021-01-12 16:07:44 +0530

    function updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const submit= () => {
-    alert("We will reachout to you soon!");
+    const name=document.getElementById("nameInput").nodeValue;
+    console.log(name);
+    alert("Hello "+name+" We will reachout to you soon!");
 }
